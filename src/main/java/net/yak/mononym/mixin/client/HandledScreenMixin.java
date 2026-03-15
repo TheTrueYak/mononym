4a29c11070589664dc9335d9e7afab7fa7a2236c
TheTrueYak <107325508+TheTrueYak@users.noreply.github.com>
2026-03-15 04:35:02 -0500

    cleanup

--- src/main/java/net/yak/mononym/mixin/client/HandledScreenMixin.java
+++ src/main/java/net/yak/mononym/mixin/client/HandledScreenMixin.java
@@ -2,10 +2,7 @@ package net.yak.mononym.mixin.client;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mojang.blaze3d.pipeline.RenderPipeline;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -13,23 +10,19 @@ import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.tooltip.HoveredTooltipPositioner;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import net.yak.mononym.Mononym;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
 
@@ -38,8 +31,6 @@ public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen
 
     @Shadow @Final protected T handler;
     @Shadow @Nullable protected Slot focusedSlot;
-
-    @Shadow @org.jspecify.annotations.Nullable protected abstract Slot getSlotAt(double mouseX, double mouseY);
 
     @Unique private static final Identifier SELECTION_OVERLAY = Mononym.id("selection_overlay");
 
